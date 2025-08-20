231ebd1244866c83313efb2a1627c5591005baac
rakib2299 <mdrakib@combosoft.co.uk>
2025-08-20 16:02:59 +0600

    succesfully work

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.get("/file/:filename", function (req, res) {
   fs.readFile(
     `./files/${req.params.filename}`,
     "utf-8",
-    function (err, filename) {
-      res.render("show");
+    function (err, filedata) {
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
     }
   );
 });
